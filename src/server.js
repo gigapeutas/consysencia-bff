40dd7ed92b7f36ae443f4e09e68563f804fd192f
gigapeutas <gigapeutas@gmail.com>
2026-02-28 03:15:49 -0300

    Atualizar o server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
-// ROTA MÁGICA: Transforma HTTP em HTTPS para o navegador não bloquear
-app.get('/stream', async (req, res) => {
-    const { url } = req.query;
-    if (!url) return res.status(400).send("URL ausente.");
-
-    try {
-        // O Render busca o vídeo via HTTP (ele não tem as travas do navegador)
-        const response = await axios({
-            method: 'get',
-            url: decodeURIComponent(url),
-            responseType: 'stream',
-            headers: { 'User-Agent': 'Mozilla/5.0' }
-        });
-
-        // Repassa o vídeo para o seu site como se fosse dele (HTTPS garantido)
-        res.set(response.headers);
-        response.data.pipe(res);
-    } catch (e) {
-        res.status(500).send("Erro na retransmissão.");
-    }
-});
+{
+  "name": "consysencia-api",
+  "version": "3.0.0",
+  "main": "src/server.js",
+  "scripts": {
+    "start": "node src/server.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.7",
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "compression": "^1.7.4",
+    "http-proxy-middleware": "^2.0.6"
+  }
+}
